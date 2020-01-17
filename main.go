6f7c5e1ba3f7ed85a1f6ff923dbd40f7c56a1cda
EUNU <rmsidsha@gmail.com>
2020-01-17 22:01:38 +0900

    add jwt auth

--- main.go
+++ main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"os"
 	"net/http"
 	"strconv"
 	"github.com/gin-gonic/contrib/static"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
+type Response struct {
+	Message string `json:"message"`
+}
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
 type Seo struct {
 	ID int `json:"id" binding:"required`
 	Seos int `json:"seos"`
 	Eunu string `json:"eunu", binding:"required"`
 
+}
+
+type JSONWebKeys struct {
+	Kty string `json:"kty"`
+	Kid string `json:"kid"`
+	Use string `json:"use"`
+	N string `json:"n"`
+	E string `json:e"`
+	X5c []string `json:x5c"`
 }
 
 // Create List of Seo
@@ -24,7 +46,36 @@ var seos = []Seo{
 	Seo{6, 0, "This is 6"},
 	Seo{7, 0, "This is 7"},
 }
+
+var jwtMiddleWare *jwtmiddleware.JWTMiddleware
+
 func main() {
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			aud := os.Getenv("AUTHO_API_AUDIENCE")
+			checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+			if !checkAudience {
+				return token, errors.New("invalid audience.")
+			}
+		// verify iss claim
+		iss := os.Getenv("AUTHO_DOMAIN")
+		checkIss := token.Claims.(jwt.Mapclaims).VerifyIssuer(iss, false)
+		if !checkIss {
+			return token, errors.New("Invalid issuer")
+		}
+
+		cert, err := getPemCert(token)
+		if err != nil {
+			log.Fatalf("could not get cert: %+v", err)
+		}
+
+		result, _ := jwt.ParseRSAPublicFromPEM([]byte(cert))
+		return result, nil
+	},
+		SigningMethod: jwt.SigningMethodRS256,
+	})
+
+	jwtMiddleware = jwtMiddleWare
 	r := gin.Default()
 	// Serve frontend static files
 	r.Use(static.Serve("/", static.LocalFile("./views", true)))
@@ -38,12 +89,55 @@ func main() {
 			})
 		})
 	}
-	api.GET("/seo", SeoHandler)   // /api/seo
-	api.POST("/seo/eunu/:seoID", LikeSeo) // /api/seo/eunu/
+	api.GET("/seo", authMiddleware(), SeoHandler)   // /api/seo
+	api.POST("/seo/eunu/:seoID", authMiddleware(), LikeSeo) // /api/seo/eunu/
 
 	r.Run() // localhost:8080
 }
 
+func getPemCert(token *jwt.Token) (string, error) {
+	cert := ""
+	resp, err := http.Get(os.Getenv("AUTHO_DOMAIN") + ".well-known/jwks.joson")
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	x5c := jwks.Keys[0].X5c
+	for k, v := range x5c {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----------BEGIN CERTIFICATE-----------\n" + v + "\n-----------END CRETIFICATE--------"
+		}
+	}
+
+	if cert == "" {
+		return cert, errors.New("unable to find appropriate key.")
+	}
+
+	return cert, nil
+}
+// authMiddleware intercepts the requests, and check for a valid jwt token
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get the client secret key
+		err := jwtMiddleWare.CheckJWT(c.Writer, c.Request)
+		if err != nil {
+			//Token not found
+			fmt.Println(err)
+			c.Abort()
+			c.Writer.WriteHeader(http.StatusUnauthorized)
+			c.Writer.Write([]byte("Unauthorized"))
+			return
+		}
+	}
+}
 func SeoHandler(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
 	c.JSON(http.StatusOK, seos)
--- views/app.jsx
+++ views/app.jsx
@@ -1,39 +1,131 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const AUTH0_CLIENT_ID = "XK7W9QQMlpO930CHf2q2a78OZQunCKn3";
+const AUTH0_DOMAIN = "dev-3q2vmqzp.auth0.com";
+const AUTH0_CALLBACK_URL = location.href;
+const AUTH0_API_AUDIENCE = "golang-gin";
 
 class App extends React.Component {
+    parseHash(){
+        this.auth0 = new this.auth0.WebAuth({
+            domain: AUTH0_DOMAIN,
+            clientID: AUTH0_CLIENT_ID,
+        });
+        this.auth0.parseHash(window.location.hash, (err, authResult) => {
+            if (err) {
+                return console.log(err);
+            }
+            if (
+                authResult !== null &&
+                authResult.accessToken !== null &&
+                authResult.idToken !==null
+            ) {
+                localStorage.setItem("access_token", authResult.accessToken);
+                localStorage.setItem("id_token", authResult.idToken);
+                localStorage.setItem(
+                    "profile",
+                    JSON.stringify(authResult.idTokenPayload)
+                );
+                window.locatio = window.location.href.suvstr(
+                    0,
+                    window.location.href.indexOf("#")
+                );
+            }
+        });
+    }
+    setup() {
+        $.ajaxSetup({
+            beforeSend: (r) => {
+                if (localStorage.getItem("access_token")) {
+                    r.setRequestHeader(
+                        "Authorization",
+                        "Bearer " + localStorage.getItem("access_token")
+                    );
+                }
+            }
+        });
+    }
+    setState() {
+        let idToken = localStorage.getItem("id_token");
+        if (idToken) {
+            this.loggedIn = true;
+        } else {
+            this.loggedIn = false;
+        }
+    }
+
+    componentWillMount() {
+        this.setup();
+        this.parseHash();
+        this.setState();
+    }
+
     render() {
         if (this.loggedIn) {
             return (<LoggedIn />);
         } else {
-            return (<Home />)
+            return (<Home />);
         }
     }
 }
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.authenticate = this.authenticate.bind(this);
+    }
+    authenticate() {
+        this.WebAuth = new auth0.WebAuth({
+            domain: AUTH0_DOMAIN,
+            clientID: AUTH0_CLIENT_ID,
+            scope: "openid profile",
+            audience: AUTH0_API_AUDIENCE,
+            responseType: "token id_token",
+            redirectUri: AUTH0_CALLBACK_URL
+        });
+        this.WebAuth.authorize();
+    }
     render() {
         return (
-            <div className ="container">
-                <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
-                    <h1>Seo Eunu</h1>
-                    <p>진짜 될까</p>
-                    <p>Sign in to get access</p>
-                    <a onClick={this.authenticate} className="btn btn-primary btn-lg btn login btn-block">sign in</a>
-                </div>
-            </div>
-        )
+            <div className="container">
+        <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
+          <h1>Seo Eunu</h1>
+          <p>This is my web</p>
+          <p>sign in to get access</p>
+          <a onClick={this.authenticate} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+        </div>
+      </div>
+        );
     }
 }
 
 class LoggedIn extends React.Component {
     constructor(props) {
-        super(props) 
-            super(props);
-            this.state = {
-                seos:[]
-            }
-        }
+        super(props);
+        this.state = {
+            seos:[]
+        };
+
+        this.serverRequest = this.serverRequest.bind(this);
+        this.logout = this.logout.bind(this);
+    }
+
+    logout() {
+        localStorage.removeItem("id_token");
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("profile");
+        location.reload();
+    }
+
+    serverRequest() {
+        $.get("http://localhost:8080/api/seo", res => {
+            this.setState({
+                seos:res
+            });
+        });
+    }
+
+    componentDidMount() {
+        this.serverRequest();
+    }
 
     render() {
         return (
@@ -50,20 +142,33 @@ class LoggedIn extends React.Component {
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
 class Seo extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             seos: ""
         }
         this.seo = this.seo.bind(this);
+        this.serverRequest = this.serverRequest.bind(this);
     }
     seo(){
-        //block
+        let seo = this.props.joke;
+        this.serverRequest(seo);
+    }
+    serverRequest(seo) {
+        $.post(
+            "http://localhost:8080/api/seo/eunu/" + seo.id,
+            { seos: 1},
+            res => {
+                console.log("res.... ", res);
+                this.setState({ eunu: "eunu!", seos: res});
+                this.props.seos = res;
+            }
+        );
     }
     render() {
         return (
@@ -81,7 +186,7 @@ class Seo extends React.Component {
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
